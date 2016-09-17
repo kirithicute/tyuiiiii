c6674a77c2166a2cc61b140fb8d5f1aaecd44946
kirithicute <kirithibaskar@gmail.com>
2016-09-17 15:06:58 +0530

    Create h.java

---
+++ h.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a day : ");
+        String22 day=s.next();
+        switch(day.toLowerCase()){
+            case "sunday":System.out.println("WorkingDay("+day+") : "+false);break;
+            default:System.out.println("WorkingDay("+day+") : "+true);
+        }
+    }
+}
+    
